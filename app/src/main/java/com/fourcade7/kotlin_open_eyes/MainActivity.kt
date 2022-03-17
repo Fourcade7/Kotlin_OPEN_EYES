264c1f2beb4d973f3dd2e54c485a05b954de2b04
Fourcade7 <fourcadeaminov7@gmail.com>
2022-03-17 17:21:38 +0500

    menu added

--- app/src/main/java/com/fourcade7/kotlin_open_eyes/MainActivity.kt
+++ app/src/main/java/com/fourcade7/kotlin_open_eyes/MainActivity.kt
@@ -2,10 +2,16 @@ package com.fourcade7.kotlin_open_eyes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mymenu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
