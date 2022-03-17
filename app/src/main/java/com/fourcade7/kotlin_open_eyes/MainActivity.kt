70ac25fefee7e399139aa72b488f04a883777e0a
Fourcade7 <fourcadeaminov7@gmail.com>
2022-03-17 16:24:29 +0500

    Initial commit

---
+++ app/src/main/java/com/fourcade7/kotlin_open_eyes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fourcade7.kotlin_open_eyes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
